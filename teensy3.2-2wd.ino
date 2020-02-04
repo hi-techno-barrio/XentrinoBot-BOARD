2a433f12e92e9d4d9dddf86ca95f77cd0fe552db
Christopher Coballes <cobecoballes@gmail.com>
2020-02-04 22:53:54 +0800

    Update teensy3.2-2wd.ino

--- teensy3.2-2wd.ino
+++ teensy3.2-2wd.ino
@@ -1,24 +1,31 @@
+/*
+ * Teensy 3.1/3.2/4.0
+ * Test program
+ * Hi-Techno Barrio
+ * 
+ */
+
 #define BRAKE 0
 #define CW    1
 #define CCW   2
 #define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example").
 
 //MOTOR 1
-#define MOTOR_A1_PIN 34 //7
-#define MOTOR_B1_PIN 36 //8
+#define MOTOR_A1_PIN 2 
+#define MOTOR_B1_PIN 0 
 
 //MOTOR 2
-#define MOTOR_A2_PIN 32//4
-#define MOTOR_B2_PIN 38 //9
+#define MOTOR_A2_PIN 5
+#define MOTOR_B2_PIN 1
 
-#define PWM_MOTOR_1 46 //5
-#define PWM_MOTOR_2 44 //6
+#define PWM_MOTOR_1 4
+#define PWM_MOTOR_2 3
 
 #define CURRENT_SEN_1 A2
-#define CURRENT_SEN_2 A0 //A3
+#define CURRENT_SEN_2 A0 
 
-#define EN_PIN_1 A6//A0
-#define EN_PIN_2 A4 //A1
+#define EN_PIN_1 7 
+#define EN_PIN_2 8  
 
 #define MOTOR_1 0
 #define MOTOR_2 1
