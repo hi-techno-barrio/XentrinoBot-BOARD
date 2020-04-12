1d0fb2e6cf69fe6561a623e96b30c40ebc3ade59
Christopher Coballes <cobecoballes@gmail.com>
2020-04-12 10:46:42 +0800

    Update Teensy-Moto-2WD.h

--- XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.h
+++ XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.h
@@ -11,12 +11,12 @@
 #define H1_5  11
 #define H1_6  12
 
-#define H2_1  20
-#define H2_2  17
-#define H2_3  16
-#define H2_4  15
-#define H2_5  14
-#define H2_6  13 
+#define ICSP_1  Vbat
+#define ICSP_2  GND
+#define ICSP_3  3.3V
+#define ICSP_4  Program
+#define ICSP_5  14/DAC
+#define ICSP_6  NC
 
 #define H2_1  20
 #define H2_2  17
