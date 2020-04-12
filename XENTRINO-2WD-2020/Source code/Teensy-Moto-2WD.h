6b13f6e4050036e5958d74e19398577eae7b2109
Christopher Coballes <cobecoballes@gmail.com>
2020-04-12 10:44:55 +0800

    Update Teensy-Moto-2WD.h

--- XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.h
+++ XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.h
@@ -1,10 +1,26 @@
 
 
 
-Header 1
+//Header 1
+// Teensy LC/3.2/4.0 
 
-#define H2_1
-#define H2_2
-#define H2_3
-#define H2_4
-#define H2_5
+#define H1_1  NC
+#define H1_2  NC
+#define H1_3  9
+#define H1_4  10
+#define H1_5  11
+#define H1_6  12
+
+#define H2_1  20
+#define H2_2  17
+#define H2_3  16
+#define H2_4  15
+#define H2_5  14
+#define H2_6  13 
+
+#define H2_1  20
+#define H2_2  17
+#define H2_3  16
+#define H2_4  15
+#define H2_5  14
+#define H2_6  13 
