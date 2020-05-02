a987a01b5442bb3b45c88dee1799fd5859cc34fa
Christopher Coballes <cobecoballes@gmail.com>
2020-05-01 23:19:50 -0700

    Update NATHANIELBOT-TEENSY-DRV8833-2WD.ino

--- NATHANIELBOT-2020/NATHANIELBOT-TEENSY-DRV8833-2WD.ino
+++ NATHANIELBOT-2020/NATHANIELBOT-TEENSY-DRV8833-2WD.ino
@@ -23,7 +23,8 @@ void setup()
   pinMode(MOTOR_A2_PIN, OUTPUT);
   pinMode(MOTOR_B2_PIN, OUTPUT);
 
- Serial.begin(9600);              // Initiates the serial to do the monitoring 
+
+  Serial.begin(115200);              // Initiates the serial to do the monitoring 
   Serial.println("Begin motor control");
   Serial.println(); //Print function list for user selection
   Serial.println("Enter number for control option:");
@@ -41,40 +42,144 @@ void loop()
 {
   char  user_input;   
   String serialReceived;
-   
+  
+  
   while(Serial.available())
   {
-   serialReceived = Serial.readStringUntil('\n');
-   user_input = serialReceived.charAt(0);
+      serialReceived = Serial.readStringUntil('\n');
+      user_input = serialReceived.charAt(0);
+
 
     digitalWrite(EN_PIN_1, HIGH);
-    digitalWrite(EN_PIN_2, HIGH); 
+    digitalWrite(EN_PIN_2, HIGH);
+     
+     switch(user_input)
+        {
+          case '1':
+          Stop();
+          break;
+          
+          case '2':
+          Forward();
+          break;
+          
+          case '3':
+           Reverse();
+          break;
+          
+          case  '+':
+          IncreaseSpeed();
+          break;
+          
+          case  '-' :
+           DecreaseSpeed();
+          break ;
+          
+          default:
+            Serial.println("Invalid option entered.");
+          break;
+        } // switch
+      
+  }  
+}
 
-    if (user_input== '1')
-    { 
-     Stop();
-    }
-    else
-    if (user_input== '2')
-    { 
-     Forward();
-    }
-    
-    else if(user_input =='3')
-    {
-      Reverse();
-    }
-    else if(user_input =='+')
-    {
-      IncreaseSpeed();
-    }
-    else if(user_input =='-')
-    {
-      DecreaseSpeed();
-    }
-    else
-    {
-      Serial.println("Invalid option entered.");
+void Stop()
+{
+  Serial.println("Stop");
+  usMotor_Status = BRAKES;
+  motorGo(MOTOR_1, usMotor_Status, 0);
+  motorGo(MOTOR_2, usMotor_Status, 0);
+}
+
+void Forward()
+{
+  Serial.println("Forward");
+  usMotor_Status = CW;
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);
+}
+
+void Reverse()
+{
+  Serial.println("Reverse");
+  usMotor_Status = CCW;
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);
+}
+
+void IncreaseSpeed()
+{
+  usSpeed = usSpeed + 10;
+  if(usSpeed > 255)
+  {
+    usSpeed = 255;  
+  }
+  
+  Serial.print("Speed +: ");
+  Serial.println(usSpeed);
+
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);  
+}
+
+void DecreaseSpeed()
+{
+  usSpeed = usSpeed - 10;
+  if(usSpeed < 0)
+  {
+    usSpeed = 0;  
+  }
+  
+  Serial.print("Speed -: ");
+  Serial.println(usSpeed);
+
+  motorGo(MOTOR_1, usMotor_Status, usSpeed);
+  motorGo(MOTOR_2, usMotor_Status, usSpeed);  
+}
+
+void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)         //Function that controls the variables: motor(0 ou 1), direction (cw ou ccw) e pwm (entra 0 e 255);
+{
+  switch (motor)
+  {
+    case  MOTOR_1 :
+            if(direct == CW)
+            {
+              digitalWrite(MOTOR_A1_PIN, LOW); 
+              digitalWrite(MOTOR_B1_PIN, HIGH);
+            }
+            else if(direct == CCW)
+            {
+              digitalWrite(MOTOR_A1_PIN, HIGH);
+              digitalWrite(MOTOR_B1_PIN, LOW);      
+            }else 
+            if (direct == BRAKES)
+            {
+            digitalWrite(MOTOR_A1_PIN, LOW);
+            digitalWrite(MOTOR_B1_PIN, LOW);
+            }
+            analogWrite(PWM_MOTOR_1, pwm); 
+    break;
+
+    case MOTOR_2 :
+           if(direct == CW)
+            {
+              digitalWrite(MOTOR_A2_PIN, LOW); 
+              digitalWrite(MOTOR_B2_PIN, HIGH);
+            }
+            else if(direct == CCW)
+            {
+              digitalWrite(MOTOR_A2_PIN, HIGH);
+              digitalWrite(MOTOR_B2_PIN, LOW);      
+            }else 
+            if (direct == BRAKES)
+            {
+            digitalWrite(MOTOR_A2_PIN, LOW);
+            digitalWrite(MOTOR_B2_PIN, LOW);
+            }
+            analogWrite(PWM_MOTOR_2, pwm); 
+    break;
+  } // switches
+}   Serial.println("Invalid option entered.");
     }
       
   }
