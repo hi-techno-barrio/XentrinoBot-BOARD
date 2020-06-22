64a7243e3cc72c686e6c9c5e1ec728fb0f0988b4
Christopher Coballes <cobecoballes@gmail.com>
2020-06-22 04:00:19 -0400

    Update Teensy-Moto-2WD.ino

--- XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.ino
+++ XENTRINO-2WD-2020/Source code/Teensy-Moto-2WD.ino
@@ -3,51 +3,28 @@ Christopher M Coballes
 Hi-Techno Barrio
 */
 
-#define BRAKES 0
-#define CW    1
-#define CCW   2
-#define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example").
+#define ENCODER_OPTIMIZE_INTERRUPTS // comment this out on Non-Teensy boards
+#include "xentrino_base_config.h"
+#include "xentrino.h"
+#include "Encoder.h"
 
-//MOTOR 1
-#define MOTOR_A1_PIN 2
-#define MOTOR_B1_PIN 1 
-
-//MOTOR 2
-#define MOTOR_A2_PIN  5
-#define MOTOR_B2_PIN  0
-
-#define PWM_MOTOR_1 4 
-#define PWM_MOTOR_2 3 
-
-#define CURRENT_SEN_1 A2
-#define CURRENT_SEN_2 A3
-
+#define set_PWM 10
+//#define MAX_RPM  100
 #define EN_PIN_1 7
 #define EN_PIN_2 8
 
-#define MOTOR_1 0
-#define MOTOR_2 1
+#define MOTOR_1  0
+#define MOTOR_2  1
+#define BRAKES   0
+#define CW       1
+#define CCW      2
+#define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example")
 
 short usSpeed = 150;  //default motor speed
 unsigned short usMotor_Status = BRAKES;
  
 void setup()                         
 {
-  pinMode(MOTOR_A1_PIN, OUTPUT);
-  pinMode(MOTOR_B1_PIN, OUTPUT);
-
-  pinMode(MOTOR_A2_PIN, OUTPUT);
-  pinMode(MOTOR_B2_PIN, OUTPUT);
-
-  pinMode(PWM_MOTOR_1, OUTPUT);
-  pinMode(PWM_MOTOR_2, OUTPUT);
-
-  pinMode(CURRENT_SEN_1, OUTPUT);
-  pinMode(CURRENT_SEN_2, OUTPUT);  
-
-  pinMode(EN_PIN_1, OUTPUT);
-  pinMode(EN_PIN_2, OUTPUT);
-
   Serial.begin(115200);              // Initiates the serial to do the monitoring 
   Serial.println("Begin motor control");
   Serial.println(); //Print function list for user selection
@@ -59,7 +36,6 @@ void setup()
   Serial.println("+. INCREASE SPEED");
   Serial.println("-. DECREASE SPEED");
   Serial.println();
-
 }
 
 void loop() 
