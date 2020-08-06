4ed28c9f7df9da24c79de124a1c29e528423aad5
Christopher Coballes <cobecoballes@gmail.com>
2020-08-06 22:16:23 +0800

    Add files via upload

---
+++ XENTRINOBOT-4WD-2020/MEGA-PRO-MINI/Xentrino_lib/xentrino.h
@@ -0,0 +1,138 @@
+#ifndef XENTRINO_H
+#define XENTRINO_H
+#include "Arduino.h"
+
+
+class Kinematics
+{
+    public:
+	 enum base {DIFFERENTIAL_DRIVE, SKID_STEER, OMNI, MECANUM};
+        base base_platform;
+		base robot_base;
+		
+        struct rpm
+        {
+            int motor1;
+            int motor2;
+            int motor3;
+            int motor4;
+        };
+        
+        struct velocities
+        {
+            float linear_x;
+            float linear_y;
+            float angular_z;
+        };
+
+        struct pwm
+        {
+            int motor1;
+            int motor2;
+            int motor3;
+            int motor4;
+        };
+
+        Kinematics(int motor_max_rpm, float wheel_diameter, float wheels_x_distance, float wheels_y_distance);    
+        velocities getVelocities(int rpm1, int rpm2,int rpm3, int rpm4);
+        rpm expected_RPM(float linear_x, float linear_y, float angular_z);
+	
+		private:
+        int max_rpm;
+        float wheels_x_distance;
+        float wheels_y_distance;
+        //float pwm_res_;
+        float wheel_circumference;
+        int total_wheels;
+
+};
+
+
+class Controller
+{
+    public:
+        enum driver {L298P,MOTO,MOTO1,MOTO2, BIG_MOTO};
+        Controller(driver motor_driver, int pwm_pin, int motor_pinA, int motor_pinB);
+        void spin(int pwm);
+		void run(int pwm);
+		
+
+   private:
+      private:   
+        void enableMOTOR(driver motor_driver);  
+		driver motor_driver_;
+        int pwm_pin_;
+        int motor_pinA_;
+        int motor_pinB_;      
+};
+
+class PID
+{
+    public:
+        PID(float min_val, float max_val, float kp, float ki, float kd);
+        double compute(float setpoint, float measured_value);
+        void updateConstants(float kp, float ki, float kd);
+
+    private:
+        float min_val_;
+        float max_val_;
+        float kp_;
+        float ki_;
+        float kd_;
+        double integral_;
+        double derivative_;
+        double prev_error_;
+};
+
+
+class Velocity 
+{
+public:
+    Velocity(int counts_per_rev );
+	int getRPM (long ticks);
+	
+private:
+	int  ticks_ ;
+	int counts_per_rev_;
+	unsigned long prev_update_time_;
+    long prev_encoder_ticks_;
+};
+
+
+class Kalman {
+
+public:
+     Kalman(float N_angle,float N_bias, float N_measure, float setAngle,float setBias);
+    // The angle should be in degrees and the rate should be in degrees per second and the delta time in seconds
+    float getAngle(float newAngle, float newRate, float dt);	
+    struct tuner
+        {
+           float Q_angle; // Process noise variance for the accelerometer
+           float Q_bias; // Process noise variance for the gyro bias
+           float R_measure; // Measurement noise variance - this is actually the variance of the measurement noise
+        };
+
+    /* These are used to tune the Kalman filter */
+	/**
+     * setQbias(float Q_bias)
+     * Default value (0.003f) is in Kalman.cpp. 
+     * Raise this to follow input more closely,
+     * lower this to smooth result of kalman filter.
+     */
+    void setFilterNoises( float Q_angle, float Q_bias, float R_measure);
+	void getFilterNoises( );
+    	
+private:
+    /* Kalman filter variables */
+    float Q_angle; // Process noise variance for the accelerometer
+    float Q_bias; // Process noise variance for the gyro bias
+    float R_measure; // Measurement noise variance - this is actually the variance of the measurement noise
+
+    float angle; // The angle calculated by the Kalman filter - part of the 2x1 state vector
+    float bias; // The gyro bias calculated by the Kalman filter - part of the 2x1 state vector
+    float rate; // Unbiased rate calculated from the rate and the calculated bias - you have to call getAngle to update the rate
+
+    float P[2][2]; // Error covariance matrix - This is a 2x2 matrix
+};
+
+#endif
---
+++ XENTRINOBOT-4WD-2020/MEGA-PRO-MINI/Xentrino_lib/xentrino_base_config.h
@@ -0,0 +1,199 @@
+#ifndef XENTRINO_BASE_CONFIG_H
+#define XENTRINO_BASE_CONFIG_H
+
+#define DEBUG 1
+
+#define K_P 0.6    // P constant
+#define K_I 0.65  //  // I constant
+#define K_D 0.5    // D constant
+
+//define your robot' specs here
+#define MAX_RPM 105                // motor's maximum RPM
+#define COUNTS_PER_REV 6533        // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.127       // wheel's diameter in meters
+#define PWM_BITS 8                 // PWM Resolution of the microcontroller
+#define LR_WHEELS_DISTANCE 0.2032  // distance between left and right wheels
+#define FR_WHEELS_DISTANCE 0.254   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
+#define MAX_STEERING_ANGLE 0.415   // max steering angle. This only applies to Ackermann steering
+
+// #define ENCODER_OPTIMIZE_INTERRUPTS // comment this out on Non-Teensy boards
+#define IMU_PUBLISH_RATE 20 //hz
+#define COMMAND_RATE 20 //hz
+#define DEBUG_RATE 5
+
+//uncomment the base you're building
+#define XENTRINO_BASE DIFFERENTIAL_DRIVE // 2WD and Tracked robot w/ 2 motors
+// #define XENTRINO_BASE SKID_STEER      // 4WD robot
+// #define XENTRINO_BASE ACKERMANN       // 2WD Car-like steering robot w/ 2 motors
+// #define XENTRINO_BASE ACKERMANN1      // 1WD Car-like steering robot w/ 1 motor
+// #define XENTRINO_BASE MECANUM         // 4WD Mecanum drive robot
+
+//uncomment the motor driver you're using
+//#define TEENSY_2WD_DRIVER
+//#define TEENSY_4WD_DRIVER
+//#define L298P_2WD_DRIVER
+#define MEGA_4WD_DRIVER
+
+//uncomment the IMU you're using
+// #define USE_GY85_IMU
+#define USE_MPU6050_IMU
+// #define USE_MPU9150_IMU
+// #define USE_MPU9250_IMU
+
+/*
+ROBOT ORIENTATION
+         FRONT
+    MOTOR1  MOTOR2  (2WD/ACKERMANN)
+    MOTOR3  MOTOR4  (4WD/MECANUM)  
+         BACK
+*/
+
+
+//TEENSY_2WD_DRIVER
+#ifdef L298P_2WD_DRIVER
+  #define MOTOR_DRIVER L298P
+  
+// ENCODER 1
+  #define MOTOR1_ENCODER_A  2
+  #define MOTOR1_ENCODER_B  3
+// ENCODER 2
+  #define MOTOR2_ENCODER_A   5   // inverted
+  #define MOTOR2_ENCODER_B   4
+
+// MOTOR 1
+  #define MOTOR1_PWM  9   // D5 PWM  motor 1
+  #define MOTOR1_IN_A 7   // D7 Clock wise Motor 1
+  #define MOTOR1_IN_B 8   // D8 Counter clock wise Motor 1
+  
+// MOTOR 2
+  #define MOTOR2_PWM  11   // D6 PWM motor 2
+  #define MOTOR2_IN_A 10   // D4 Clock wise Motor 2
+  #define MOTOR2_IN_B 12   // D9 Counter clock wise Motor 2
+
+  #define MOTOR1_MOTO_EN1 20     // Activalte motor 1 
+  #define MOTOR2_MOTO_EN2  20    // Activalte motor 2 
+  #define MOTOR3_MOTO_EN3  20    // Activalte motor 1 
+  #define MOTOR4_MOTO_EN4  20    // Activalte motor 2 
+  
+  #define PWM_MAX pow(2, PWM_BITS) - 1  //255
+  #define PWM_MIN -PWM_MAX
+#endif 
+
+//TEENSY_2WD_DRIVER
+#ifdef TEENSY_2WD_DRIVER
+  #define MOTOR_DRIVER MOTO
+  
+// ENCODER 1
+  #define MOTOR1_ENCODER_A  23
+  #define MOTOR1_ENCODER_B  22
+// ENCODER 2
+  #define MOTOR2_ENCODER_A   6   // inverted
+  #define MOTOR2_ENCODER_B   21
+
+// MOTOR 1
+  #define MOTOR1_PWM  4   // D5 PWM  motor 1
+  #define MOTOR1_IN_A 2   // D7 Clock wise Motor 1
+  #define MOTOR1_IN_B 1   // D8 Counter clock wise Motor 1
+  
+// MOTOR 2
+  #define MOTOR2_PWM  3   // D6 PWM motor 2
+  #define MOTOR2_IN_A 5   // D4 Clock wise Motor 2
+  #define MOTOR2_IN_B 0   // D9 Counter clock wise Motor 2
+
+  #define MOTOR1_MOTO_EN1  7    // Activalte motor 1 
+  #define MOTOR2_MOTO_EN2  8    // Activalte motor 2 
+
+  
+  #define PWM_MAX pow(2, PWM_BITS) - 1  //255
+  #define PWM_MIN -PWM_MAX
+#endif 
+
+//TEENSY_4WD_DRIVER
+#ifdef TEENSY_4WD_DRIVER
+  #define MOTOR_DRIVER MOTO1
+    
+// ENCODER 1
+  #define MOTOR1_ENCODER_A 21
+  #define MOTOR1_ENCODER_B 6 
+// ENCODER 2
+  #define MOTOR2_ENCODER_A 22
+  #define MOTOR2_ENCODER_B 23
+// ENCODER 3  
+  #define MOTOR3_ENCODER_A 30
+  #define MOTOR3_ENCODER_B 30 
+// ENCODER 4
+  #define MOTOR4_ENCODER_A 30
+  #define MOTOR4_ENCODER_B 30
+  
+// MOTOR 1
+  #define MOTOR1_PWM  21
+  #define MOTOR1_IN_A 7
+  #define MOTOR1_IN_B 8
+// MOTOR 2
+  #define MOTOR2_PWM  22
+  #define MOTOR2_IN_A 20
+  #define MOTOR2_IN_B 23
+// MOTOR 3
+  #define MOTOR3_PWM  3
+  #define MOTOR3_IN_A 0
+  #define MOTOR3_IN_B 1
+// MOTOR 4
+  #define MOTOR4_PWM  4
+  #define MOTOR4_IN_A 2
+  #define MOTOR4_IN_B 5
+  
+// ENABLE MOTORS  
+  #define MOTOR1_MOTO1_EN1 13
+  #define MOTOR2_MOTO1_EN2 13
+  #define MOTOR3_MOTO1_EN3  6   
+  #define MOTOR4_MOTO1_EN4  6
+   
+  #define PWM_MAX pow(2, PWM_BITS) - 1
+  #define PWM_MIN -PWM_MAX
+#endif 
+
+
+// MEGA_4WD_DRIVER
+ #ifdef MEGA_4WD_DRIVER
+  #define MOTOR_DRIVER MOTO2
+
+/// ENCODER  PINS
+  #define MOTOR1_ENCODER_A 3
+  #define MOTOR1_ENCODER_B 15 
+
+  #define MOTOR2_ENCODER_A 2
+  #define MOTOR2_ENCODER_B 17
+
+  #define MOTOR3_ENCODER_A 40
+  #define MOTOR3_ENCODER_B 18 
+
+  #define MOTOR4_ENCODER_A 42
+  #define MOTOR4_ENCODER_B 19
+
+// MOTOR 1
+  #define MOTOR1_PWM  46
+  #define MOTOR1_IN_A 34
+  #define MOTOR1_IN_B 36
+// MOTOR 2
+  #define MOTOR2_PWM  44
+  #define MOTOR2_IN_A 32
+  #define MOTOR2_IN_B 38
+// MOTOR 3
+  #define MOTOR3_PWM  6
+  #define MOTOR3_IN_A 7
+  #define MOTOR3_IN_B 9
+// MOTOR 4
+  #define MOTOR4_PWM  5
+  #define MOTOR4_IN_A 8
+  #define MOTOR4_IN_B 11
+  
+  #define MOTOR1_MOTO_EN1 A6   // activalte motor  1 
+  #define MOTOR2_MOTO_EN2 A4   // activalte motor  2 
+  #define MOTOR3_MOTO_EN3 A15  // activalte motor 3
+  #define MOTOR4_MOTO_EN4 A13  // activalte motor 4 
+  
+  #define PWM_MAX pow(2, PWM_BITS) - 1
+  #define PWM_MIN -PWM_MAX
+#endif 
+
+#endif
